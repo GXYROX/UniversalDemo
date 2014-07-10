34f3b4300ec1b2b9fa775a49bbeb1d7015cf8f98
teenstian@gmail.com <teenstian@gmail.com>
2014-07-10 13:35:34 +0800

    A New Demo

---
+++ UniversalDemo/src/com/daimajia/androidanimations/library/fading_exits/FadeOutUpAnimator.java
@@ -0,0 +1,19 @@
+package com.daimajia.androidanimations.library.fading_exits;
+
+import android.animation.ObjectAnimator;
+import android.view.View;
+
+import com.daimajia.androidanimations.library.BaseViewAnimator;
+
+/**
+ * Created by daimajia on 14-6-21.
+ */
+public class FadeOutUpAnimator extends BaseViewAnimator {
+    @Override
+    public void prepare(View target) {
+        getAnimatorAgent().playTogether(
+                ObjectAnimator.ofFloat(target, "alpha", 1, 0),
+                ObjectAnimator.ofFloat(target,"translationY",0,-20)
+        );
+    }
+}
