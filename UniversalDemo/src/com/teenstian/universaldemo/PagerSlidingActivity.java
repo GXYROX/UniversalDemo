b6722e5edbf4f39da47bd2ce5d4e2f7d6283c54f
teenstian@gmail.com <teenstian@gmail.com>
2014-07-10 13:33:55 +0800

    remove all

--- UniversalDemo/src/com/teenstian/universaldemo/PagerSlidingActivity.java
+++
@@ -1,7 +0,0 @@
-package com.teenstian.universaldemo;
-
-import android.app.Activity;
-
-public class PagerSlidingActivity extends Activity {
-
-}
--- UniversalDemo/src/com/teenstian/universaldemo/pagersliding/PagerSlidingActivity.java
+++
@@ -1,7 +0,0 @@
-package com.teenstian.universaldemo.pagersliding;
-
-import android.app.Activity;
-
-public class PagerSlidingActivity extends Activity {
-
-}
